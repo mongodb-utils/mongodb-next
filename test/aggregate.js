1fe3b9c7740452e9b3777f1537d75e946bbdf25e
Jonathan Ong <jonathanrichardong@gmail.com>
2014-07-18 15:22:32 -0700

    merge aggregate-stream

--- test/aggregate.js
+++ test/aggregate.js
@@ -1,10 +1,94 @@
 
+var match = {
+  agg: true
+}
+
+var byValue = {
+  _id: '$value',
+  count: {
+    $sum: 1
+  }
+}
+
 describe('.aggregate()', function () {
+  before(function () {
+    return collection.insert([{
+      value: 1,
+      agg: true
+    }, {
+      value: 2,
+      agg: true
+    }, {
+      value: 3,
+      agg: true
+    }])
+  })
+
   it('.match().then()', function () {
-    return collection.aggregate().match({
-      name: 'taylor'
-    }).then(function (docs) {
-      assert.equal(1, docs.length)
+    return collection.aggregate().match(match).then(function (docs) {
+      assert.equal(3, docs.length)
+    })
+  })
+
+  it('.match().exec()', function (done) {
+    collection.aggregate().match(match).exec(function (err, docs) {
+      assert.ifError(err)
+      assert.equal(3, docs.length)
+      done()
+    })
+  })
+
+  it('.match().toArray()', function (done) {
+    collection.aggregate().match(match).toArray(function (err, docs) {
+      assert.ifError(err)
+      assert.equal(3, docs.length)
+      done()
+    })
+  })
+
+  describe('.destroy()', function () {
+    it('should emit "close"', function (done) {
+      var stream = collection.aggregate().match(match).group(byValue)
+      // execute the stream
+      stream.toArray(noop)
+
+      stream.on('close', done)
+      stream.on('error', done)
+      stream.destroy()
+    })
+
+    it('should still emit "close" if never opened', function () {
+      var stream = collection.aggregate().match(match).group(byValue)
+      var closed = false
+      stream.once('close', function () {
+        closed = true
+      })
+      stream.destroy()
+      assert(closed)
+    })
+  })
+
+  it('.explain()', function () {
+    return collection.aggregate().match(match).group(byValue).explain()
+  })
+
+  it('.map()', function () {
+    return collection.aggregate().match(match).map(transform).then(function (docs) {
+      assert.equal(3, docs.length)
+      assert(transformed(docs))
     })
   })
 })
+
+function noop() {}
+
+function transform(x) {
+  x.transformed = true
+  return x
+}
+
+function transformed(docs) {
+  return docs.every(function (x) {
+    return x.transformed
+  })
+}
